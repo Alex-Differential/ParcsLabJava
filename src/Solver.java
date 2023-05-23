f67622dadbec8566cf8651891520b5a91459ac58
Alex-Differential <aleksandr200142@gmail.com>
2023-05-23 12:52:31 +0300

    First change

--- src/Solver.java
+++ src/Solver.java
@@ -30,7 +30,7 @@ public class Solver implements AM
 
         try
         {
-            BufferedReader in = new BufferedReader(new FileReader(info.curtask.findFile("input_3_2.txt")));
+            BufferedReader in = new BufferedReader(new FileReader(info.curtask.findFile("input_0_2.txt")));
 
             lp = Integer.parseInt(in.readLine());
             la = Long.parseLong(in.readLine());
